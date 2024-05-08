ad18942b7d16a89625d600f8df4fdaab40ff2c31
siphesihle <siphesihle@L1410>
2024-05-08 14:34:27 +0200

    Add project files.

---
+++ WebApplication1/Models/Survey.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Survey
+    {
+        public string? fullName { get; set; }
+        public string? email { get; set; }
+        public DateTime madeDate { get; set; }
+        public string? contact { get; set; }
+
+        public Boolean choice { get; set; }
+        public Boolean check { get; set; }
+    }
+}
---
+++ WebApplication1/Models/SurveyContext.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace WebApplication1.Models
+{
+   public class SurveyContext 
+    {
+        // Connection string to your SQL Server database
+        private string connectionString = "Data Source=YourServer;Initial Catalog=YourDatabase;Integrated Security=True";
+
+        public void CreateTable()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    // SQL command to create a table
+                    string createTableQuery = @"CREATE TABLE Survey (
+                                                ID INT IDENTITY(1,1) PRIMARY KEY,
+                                               Name NVARCHAR(100),
+                                                Email NVARCHAR(100),
+                                                DateOfBirth DATE,
+                                                Contact NVARCHAR(20),
+                                                choice Boolean,
+                                                checked Boolean
+
+                                              )";
+
+                    using (SqlCommand command = new SqlCommand(createTableQuery, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+
+                    Console.WriteLine("Table created successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error creating table: " + ex.Message);
+            }
+        }
+    }
+
+}
+
